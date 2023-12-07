6aff39029498a8f69b667fbd7f4b0bdf7c3de5df
CarinWood <carin.wood.85@gmail.com>
2023-12-07 16:28:27 +0100

    added calculate quantity function

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,23 @@ import './header.css'
 import ninjahead from '../../assets/images/ninjahead.svg'
 import { FaShoppingCart } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom'
+import { useShoppingCart } from '../../contexts/CartContext';
 
 export const Header = () => {
   const navigate = useNavigate()
+  const {cart} = useShoppingCart()
 
 
   const toCheckoutPage = () => {
     navigate('/checkout')
+  }
+
+  const calculateQuantity = () => {
+    let sum = 0;
+     for (let i = 0; i < cart.length; i++) {
+        sum += cart[i].quantity;      
+     }
+      return sum
   }
 
 
@@ -24,7 +34,7 @@ export const Header = () => {
         <div className='cart-icon-frame' onClick={toCheckoutPage}>
 
           <FaShoppingCart className='cart-icon'/> 
-          <p className='digit'>4</p>
+          <p className='digit'>{calculateQuantity()}</p>
 
         </div>
     </header>
