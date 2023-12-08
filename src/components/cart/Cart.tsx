046467ffb116aa13c207167e2640fd8d642622d4
CarinWood <carin.wood.85@gmail.com>
2023-12-08 09:50:39 +0100

    calculate total function added

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -9,6 +9,12 @@ export const Cart = () => {
     alert('Thank you for your purchase!')
   }
 
+  const calculateTotal = () => {
+    const total = cart.reduce((total, item) => total + item.quantity * item.price, 0);
+    const formattedTotal = parseFloat(total.toFixed(2))
+        return formattedTotal;
+  }
+
  
 
   return (
@@ -20,19 +26,26 @@ export const Cart = () => {
           <p>Price</p>
           <span></span>
         </div>
+            {cart.length === 0 ?
+            <div className='no-poster-msg'>There's no posters in your cart yet :-(</div>
+            :
+            <>
             {cart.map(item => {
               return <CartProduct key={item.id} {...item}/>
             })}
+            </>
+            }
         <div className='divider'></div>
         <div className='summary'>
           <p>Total</p>
           <p></p>
-          <p>€123</p>
+          <p className='total'>€{calculateTotal()}</p>
         </div>
-
+        {cart.length > 0 &&
         <div className='gimmie-btn-area'>    
           <button className='gimmie-btn' onClick={message}>Gimmie gimmie gimmie!</button>
         </div>
+        }
     </section>
   )
 }
